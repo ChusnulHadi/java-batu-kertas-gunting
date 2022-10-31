acdde8c3e60c25eddd9597de3339acbb0951f2d4
fiqifalahdev <fiqifalah17@gmail.com>
2022-10-31 17:26:06 +0700

    TestingPlayButton

--- src/main/java/pbo/projectuas/GamesController.java
+++ src/main/java/pbo/projectuas/GamesController.java
@@ -1,21 +1,32 @@
 package pbo.projectuas;
 
 import java.io.IOException;
-import java.util.Random;
+// import java.util.Random;
 
+// import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+// import javafx.scene.image.Image;
+// import javafx.scene.control.Button;
+// import javafx.scene.image.ImageView;
 
 public class GamesController {
-    private String[] element = { "paper", "rock", "scissors" };
-
-    private String getRandomChoice(String[] choices) {
-        Random rand = new Random();
-        int choice = rand.nextInt(choices.length);
-        return choices[choice];
-    }
+    // private String[] element = { "paper", "rock", "scissors" };
 
     @FXML
-    private void play() throws IOException {
-        String choice = this.getRandomChoice(element);
+    public Button playButton;
+
+    // private String getRandomChoice(String[] choices) {
+    // Random rand = new Random();
+    // int choice = rand.nextInt(choices.length);
+    // return choices[choice];
+    // }
+
+    @FXML
+    private void buttonHandler() throws IOException {
+        playButton.setText("Kotnol");
+        // String choice = this.getRandomChoice(element);
+        // ImageView mainImage = new ImageView(new
+        // Image(getClass().getResourceAsStream("@../../" + choice + ".png")));
     }
 }
--- src/main/java/pbo/projectuas/ShuffleImage.java
+++
@@ -1,6 +0,0 @@
-package pbo.projectuas;
-public static ShuffleImage {
-    public ShuffleImage() {
-
-    }
-}

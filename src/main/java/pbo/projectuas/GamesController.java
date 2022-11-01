d987725d989d44f562eb49e084651c839356c586
fiqifalahdev <fiqifalah17@gmail.com>
2022-11-01 11:58:51 +0700

    Button Clicked will change the images

--- src/main/java/pbo/projectuas/GamesController.java
+++ src/main/java/pbo/projectuas/GamesController.java
@@ -1,30 +1,35 @@
 package pbo.projectuas;
 
-import java.io.IOException;
-// import java.util.Random;
-
-// import javafx.event.ActionEvent;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import java.util.Random;
 
 public class GamesController {
-    // private String[] element = { "paper", "rock", "scissors" };
+    private String[] element = { "paper", "rock", "scissors" };
+    @FXML
+    private AnchorPane mainStage;
 
     @FXML
-    Button playButton;
-    ImageView mainImage;
+    private ImageView myImageView;
 
-    // private String getRandomChoice(String[] choices) {
-    // Random rand = new Random();
-    // int choice = rand.nextInt(choices.length);
-    // return choices[choice];
-    // }
+    @FXML
+    private Button playButton;
 
-    public void buttonHandler() throws IOException {
-        // String choice = this.getRandomChoice(element);
-        Image image = new Image("paper.png");
-        mainImage.setImage(image);
+    private String getRandomChoice(String[] choices) {
+        Random rand = new Random();
+        int choice = rand.nextInt(choices.length);
+        return choices[choice];
     }
+
+    @FXML
+    void buttonHandler(ActionEvent event) {
+        String choice = this.getRandomChoice(this.element);
+        System.out.println(choice);
+        myImageView.setImage(new Image(choice + ".png"));
+    }
+
 }

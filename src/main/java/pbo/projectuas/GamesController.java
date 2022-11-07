d17dd347f9b38335f526eb19596613c31799ba22
ChusnulHadi <office.chusnulhadi@gmail.com>
2022-11-07 20:52:59 +0700

    adding loop image

--- src/main/java/pbo/projectuas/GamesController.java
+++ src/main/java/pbo/projectuas/GamesController.java
@@ -7,9 +7,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class GamesController {
     private String[] element = { "paper", "rock", "scissors" };
+    private Boolean isLoop = false;
     @FXML
     private AnchorPane mainStage;
 
@@ -28,8 +30,23 @@ public class GamesController {
     @FXML
     void buttonHandler(ActionEvent event) {
         String choice = this.getRandomChoice(this.element);
-        System.out.println(choice);
-        myImageView.setImage(new Image(choice + ".png"));
+        isLoop = !isLoop;
+        try {
+            imageRender();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+    @FXML
+    void imageRender() throws InterruptedException {
+        while (isLoop) {
+            String imageUrl = getRandomChoice(element);
+            System.out.println("imageUrl");
+            myImageView.setImage(new Image(imageUrl + ".png"));
+            TimeUnit.MILLISECONDS.sleep(2000);
+        }
+    }
+
 
 }

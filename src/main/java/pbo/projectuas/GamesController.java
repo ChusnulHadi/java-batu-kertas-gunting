598ee22ffe0dd74d1fe12b29875be34b14b74ec5
fiqifalahdev <fiqifalah17@gmail.com>
2022-11-08 09:29:36 +0700

    logic winner

--- src/main/java/pbo/projectuas/GamesController.java
+++ src/main/java/pbo/projectuas/GamesController.java
@@ -5,7 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
 import java.util.Timer;
 import java.util.TimerTask;
 // import java.util.Random;
@@ -13,6 +15,8 @@ import java.util.TimerTask;
 public class GamesController {
 
     private Boolean isLoop = true;
+    private int userChoice;
+    private int comp;
 
     @FXML
     private AnchorPane mainStage;
@@ -32,6 +36,9 @@ public class GamesController {
     @FXML
     private Button scissorsButton;
 
+    @FXML
+    private Label notif;
+
     private String getRandomChoice() {
         RandomImage getImage = new RandomImage();
         return getImage.randomsString();
@@ -47,6 +54,16 @@ public class GamesController {
                     @Override
                     public void run() {
                         String choice = getRandomChoice();
+                        if (choice == "paper") {
+                            comp = 1;
+                            // System.out.println(comp);
+                        } else if (choice == "rock") {
+                            comp = 2;
+                            // System.out.println(comp);
+                        } else {
+                            comp = 3;
+                            // System.out.println(comp);
+                        }
                         System.out.println(choice);
                         myImageView.setImage(new Image(choice + ".png"));
                         if (!isLoop)
@@ -60,7 +77,9 @@ public class GamesController {
     void getPaperVal(ActionEvent event) {
         isLoop = false;
         int paperInt = 1;
-        System.out.println(paperInt);
+        this.userChoice = paperInt;
+        System.out.println(this.userChoice);
+        checkWinner();
     }
 
     // mendapatkan nilai dari ketika user menekan tombol rock
@@ -68,7 +87,9 @@ public class GamesController {
     void getRockVal(ActionEvent event) {
         isLoop = false;
         int rockInt = 2;
-        System.out.println(rockInt);
+        this.userChoice = rockInt;
+        System.out.println(this.userChoice);
+        checkWinner();
     }
 
     // mendapatkan nilai dari ketika user menekan tombol scissors
@@ -76,7 +97,40 @@ public class GamesController {
     void getScissorsVal(ActionEvent event) {
         isLoop = false;
         int scissorInt = 3;
-        System.out.println(scissorInt);
+        this.userChoice = scissorInt;
+        System.out.println(this.userChoice);
+        checkWinner();
     }
 
+    // set winner
+    void checkWinner() {
+        // 1. paper
+        // 2. rock
+        // 3. scissor
+        if (this.comp == 1) {
+            if (this.userChoice == 1) {
+                notif.setText("Seri");
+            } else if (this.userChoice == 2) {
+                notif.setText("Kalah");
+            } else {
+                notif.setText("Menang");
+            }
+        } else if (this.comp == 2) {
+            if (this.userChoice == 1) {
+                notif.setText("Menang");
+            } else if (this.userChoice == 2) {
+                notif.setText("Seri");
+            } else {
+                notif.setText("Kalah");
+            }
+        } else {
+            if (this.userChoice == 1) {
+                notif.setText("Kalah");
+            } else if (this.userChoice == 2) {
+                notif.setText("Menang");
+            } else {
+                notif.setText("Seri");
+            }
+        }
+    }
 }
